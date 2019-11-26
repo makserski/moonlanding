31f881fc8e34e1e1218bc98f1fa096ef56da0856
danjohnston1298 <57505159+danjohnston1298@users.noreply.github.com>
2019-11-26 17:01:25 +0000

    Create updatetable.m

---
+++ updatetable.m
@@ -0,0 +1,11 @@
+function matrix = updatetable(s,a,v,h,angle,hh,matrix)
+     matrix(1+s,1) = a;
+
+     matrix(1+s,2) = v;
+    
+     matrix(1+s,3) = h;
+
+     matrix(1+s,4) = angle;
+
+     matrix(1+s,5) = hh;
+end
