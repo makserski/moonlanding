1dce6ce063ed8d60e35eafe80daf60c188d00482
danjohnston1298 <57505159+danjohnston1298@users.noreply.github.com>
2019-11-26 16:58:40 +0000

    Create density.m

---
+++ density.m
@@ -0,0 +1,14 @@
+function rho = density(h);
+h=h*3.28084;
+if h < 11019.13
+    temp=59-0.00356*h;
+    preassure=2116*((temp+459.7)/518.6)^5.256;
+elseif h < 25098
+    temp = -70;
+    preassure = 473.1*exp(1.73-0.000048*h);
+elseif h > 25097
+    temp = -205.05 + 0.00164*h;
+    preassure = 51.97*((temp+459.7)/389.98)^-11.388;
+end
+rho = preassure/(1718*(temp+459.7));
+end
