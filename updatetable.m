3a41ee9481ae1f4ac9d87ae5193aa2bde6d123d2
danjohnston1298 <57505159+danjohnston1298@users.noreply.github.com>
2019-11-27 18:14:21 +0000

    Update updatetable.m

--- updatetable.m
+++ updatetable.m
@@ -1,4 +1,4 @@
-function matrix = updatetable(s,a,v,h,angle,hh,totalT,matrix)
+function matrix = updatetable(s,a,v,h,angle,hh,totalT,matrix,M)
      matrix(1+s,1) = a;
 
      matrix(1+s,2) = v;
@@ -10,4 +10,6 @@ function matrix = updatetable(s,a,v,h,angle,hh,totalT,matrix)
      matrix(1+s,5) = hh;
      
      matrix(1+s,6) = totalT;
+     
+     matrix(1+s,7) = M;
 end
