17ebd4da50bd24ec4774b1ad9620ed4ecba03df2
makserski <49100139+makserski@users.noreply.github.com>
2019-11-27 15:23:53 +0000

    Add files via upload

--- translunar_injection.m
+++ translunar_injection.m
@@ -1,17 +1,19 @@
-%function [X_COORD, Y_COORD] = translunar_injection()
+%function [X_COORD, Y_COORD, VELOCITY, TIME] = translunar_injection()
 
-clear all
-
+    clear all
+    
     EARTH_ORBIT = 6569.3e3; % Parking orbit of the Earth
     MOON_ORBIT = 1848e3; % Orbit of the Moon
     EARTH_MOON_DIST = 384400e3; % Distance from the Earth to the Moon
     X_COORD(1) = 0; % Starting point at x=0
     Y_COORD(1) = 0; % Starting point at y=0
-    G = 6.673e-11;
+    G = 6.673e-11; % Gravitational constant
+    MASS_EARTH = 5.9724e24; % Mass of the Earth
+    MU = G*MASS_EARTH; % Standard gravitational parameter
     
     % Initialise the time array
-    dt = 1;
-    TIME = 0:dt:10300;
+    dt = 5;
+    TIME = 0:dt:262763;
     
     % Calculating the geometrical properties of the elliptical orbit during
     % the Hohmann transfer to the Moon
@@ -31,22 +33,25 @@ clear all
     % location of the rocket changes so its values will be found within the
     % for-loop below
     
-    dE = pi/10300;
+    dE = pi/length(TIME);
     E = 0:dE:pi; % This assumes a constant angular velocity about the centre point of the big circle
 
-    MASS_EARTH = 5.9724e24;
-    
     for i = 1:length(TIME)
+        
+        if i == length(TIME) % Making sure the number of array elements does not exceed length(TIME)
+            TRUE_ANOMALY(i) = 2*atan(sqrt((1+ECCENTRICITY)/(1-ECCENTRICITY))*tan((E(i))/2));
+            r(i) = (SEMI_MAJOR_AXIS*(1+ECCENTRICITY^2))/(1+ECCENTRICITY*cos(TRUE_ANOMALY(i)));
+        
+        else % The normal case
+            TRUE_ANOMALY(i) = 2*atan(sqrt((1+ECCENTRICITY)/(1-ECCENTRICITY))*tan((E(i))/2));
+            r(i) = (SEMI_MAJOR_AXIS*(1+ECCENTRICITY^2))/(1+ECCENTRICITY*cos(TRUE_ANOMALY(i)));
 
-        TRUE_ANOMALY(i) = 2*atan(sqrt((1+ECCENTRICITY)/(1-ECCENTRICITY)).*tan((E(i))/2));
-        r(i) = (SEMI_MAJOR_AXIS*(1+ECCENTRICITY^2))./(1+ECCENTRICITY.*cos(TRUE_ANOMALY(i)));
-        
-        X_COORD(i+1) = r(i).*(cos(TRUE_ANOMALY(i) + ARG_PERICENTRE)*cos(ASCENDING_NODE) - ...
-        sin(TRUE_ANOMALY(i) + ARG_PERICENTRE)*sin(ASCENDING_NODE)*cos(INCLINATION));
-        
-        Y_COORD(i+1) = r(i).*(cos(TRUE_ANOMALY(i) + ARG_PERICENTRE)*sin(ASCENDING_NODE) + ...
-        sin(TRUE_ANOMALY(i) + ARG_PERICENTRE)*cos(ASCENDING_NODE)*cos(INCLINATION));
-        
+            X_COORD(i+1) = r(i)*(cos(TRUE_ANOMALY(i) + ARG_PERICENTRE)*cos(ASCENDING_NODE) - ...
+            sin(TRUE_ANOMALY(i) + ARG_PERICENTRE)*sin(ASCENDING_NODE)*cos(INCLINATION));
+
+            Y_COORD(i+1) = r(i)*(cos(TRUE_ANOMALY(i) + ARG_PERICENTRE)*sin(ASCENDING_NODE) + ...
+            sin(TRUE_ANOMALY(i) + ARG_PERICENTRE)*cos(ASCENDING_NODE)*cos(INCLINATION));
+        end
         %add comments here
     
         %VELOCITY(i) = sqrt((MASS_EARTH*G).*((2/r(i))-(1/SEMI_MAJOR_AXIS)));
@@ -64,21 +69,33 @@ clear all
     for i = 1:length(Y_COORD)-1
         Y_COORD(i+1) = Y_COORD(i+1) - INITIAL_ERROR_Y;
     end
-    %figure(1)
-    %comet(X_COORD,Y_COORD)
+    
+    figure(1)
+    comet(X_COORD,Y_COORD)
+    
+    % Constants and arrays required for the velocity calculation
+    n = sqrt(MU/(SEMI_MAJOR_AXIS^3));
+    l1 = cos(ASCENDING_NODE)*cos(ARG_PERICENTRE) - sin(ASCENDING_NODE)*sin(ARG_PERICENTRE)*cos(INCLINATION);
+    l2 = - cos(ASCENDING_NODE)*sin(ARG_PERICENTRE) - sin(ASCENDING_NODE)*cos(ARG_PERICENTRE)*cos(INCLINATION);
+    m1 = sin(ASCENDING_NODE)*cos(ARG_PERICENTRE) + cos(ASCENDING_NODE)*sin(ARG_PERICENTRE)*cos(INCLINATION);
+    m2 = - sin(ASCENDING_NODE)*sin(ARG_PERICENTRE) - cos(ASCENDING_NODE)*cos(ARG_PERICENTRE)*cos(INCLINATION);
+    n1 = sin(ARG_PERICENTRE)*sin(INCLINATION);
+    n2 = cos(ARG_PERICENTRE)*sin(INCLINATION);
+    dNU = pi/(length(TIME)-1); 
+    NU = 0:dNU:pi;
     
     %VELOCITY(1) = 7.7889e+03;
-    for i = 1:length(TIME)-1
-        R(i) = sqrt((Y_COORD(i).^2)+((X_COORD(i)-EARTH_ORBIT).^2));
-        VELOCITY(i+1) = sqrt((MASS_EARTH*G).*((2/R(i))-(1/SEMI_MAJOR_AXIS)));
+    % Calculating the velocity of the spacecraft
+    for i = 1:length(TIME)
+        R(i) = sqrt((Y_COORD(i)^2)+((X_COORD(i)-EARTH_ORBIT)^2));
+%         VELOCITY(i) = sqrt((MASS_EARTH*G)*((2/R(i))-(1/SEMI_MAJOR_AXIS)));
+        X_VELOCITY(i) = ((n*SEMI_MAJOR_AXIS)/r(i))*(SEMI_MINOR_AXIS*l2*cos(NU(i)) - SEMI_MAJOR_AXIS*l1*sin(NU(i)));
+        Y_VELOCITY(i) = ((n*SEMI_MAJOR_AXIS)/r(i))*(SEMI_MINOR_AXIS*m2*cos(NU(i)) - SEMI_MAJOR_AXIS*m1*sin(NU(i)));
     end
-    figure(2)
-    %plot(TIME,VELOCITY)
+%     figure(2)
+%     plot(TIME,X_VELOCITY)
     % Draw x and y which represent the path of the rocket in 2D
-    for i = 1:length(TIME)-1
-        Vel(i+1) = sqrt((X_COORD(i+1)-X_COORD(i)).^2+((Y_COORD(i+1)-Y_COORD(i)).^2));
-    end
-    plot(TIME,Vel)
+    
+    va = sqrt(((G*MASS_EARTH)/SEMI_MAJOR_AXIS)*((1-ECCENTRICITY)/(1+ECCENTRICITY)));
+    vp = sqrt(((G*MASS_EARTH)/SEMI_MAJOR_AXIS)*((1+ECCENTRICITY)/(1-ECCENTRICITY)));
 %end
-    va = sqrt(((G*MASS_EARTH)/SEMI_MAJOR_AXIS)*((1-ECCENTRICITY)/(1+ECCENTRICITY)))
-    vp = sqrt(((G*MASS_EARTH)/SEMI_MAJOR_AXIS)*((1+ECCENTRICITY)/(1-ECCENTRICITY)))
